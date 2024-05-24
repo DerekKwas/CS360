b07a544f969a97d6e96b42a104205dce45c808a6
DerekKwas <75740508+DerekKwas@users.noreply.github.com>
2024-05-23 20:58:09 -0400

    Learned about opening new activities & passing data to them

--- zybooks/LightsOut/app/src/main/java/com/zybooks/lightsout/MainActivity.java
+++ zybooks/LightsOut/app/src/main/java/com/zybooks/lightsout/MainActivity.java
@@ -1,11 +1,17 @@
 package com.zybooks.lightsout;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -16,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private LightsOutGame mGame;
     private GridLayout mLightGrid;
     private int mLightOnColor;
+    private int mLightOnColorId;
     private int mLightOffColor;
 
     @Override
@@ -23,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mLightOnColorId = R.color.yellow;
         mLightGrid = findViewById(R.id.light_grid);
 
         // Add the same click handler to all grid buttons
@@ -92,4 +100,35 @@ public class MainActivity extends AppCompatActivity {
     public void onNewGameClick(View view) {
         startGame();
     }
+
+    public void onHelpClick(View view) {
+        Intent intent = new Intent(this, HelpActivity.class);
+        startActivity(intent);
+    }
+
+    public void onChangeColorClick(View view) {
+        // Send the current color ID to ColorActivity
+        Intent intent = new Intent(this, ColorActivity.class);
+        intent.putExtra(ColorActivity.EXTRA_COLOR, mLightOnColorId);
+        mColorResultLauncher.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> mColorResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            // Create the "on" button color from the chosen color ID from ColorActivity
+                            mLightOnColorId = data.getIntExtra(ColorActivity.EXTRA_COLOR, R.color.yellow);
+                            mLightOnColor = ContextCompat.getColor(MainActivity.this, mLightOnColorId);
+                            setButtonColors();
+                        }
+                    }
+                }
+            });
 }
+
+
